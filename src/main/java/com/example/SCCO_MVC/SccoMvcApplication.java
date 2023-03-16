e782af2a1d64a0faf72b35a9661372f48a39158f
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-03-16 07:30:22 -0300

    Primeiro commit

---
+++ src/main/java/com/example/SCCO_MVC/SccoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.SCCO_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccoMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SCCO_MVC/SccoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SCCO_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
