4f704360fb878131297e52d9fa80eae2d4d47a1e
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-03-20 19:32:14 -0300

    Atualização da classe Disponibilidade

--- src/main/java/com/example/SCCO_MVC/model/entity/Dia.java
+++ src/main/java/com/example/SCCO_MVC/model/entity/Dia.java
@@ -1,26 +1,23 @@
 package com.example.SCCO_MVC.model.entity;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Time;
-import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Disponibilidade {
+public class Dia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Time horaInicialIntervalo;
-    private Time horaFinalIntervalo;
+    private String nome;
+    private Time horaInicial;
+    private Time horaFinal;
 
-    @OneToMany
-    private List<Dia> dia;
 }
