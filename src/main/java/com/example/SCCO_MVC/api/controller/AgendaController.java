749dc51b155889a7e9e6162c271ff83823b01a6b
Giulianobrjf <giuliano.leite2002@gmail.com>
2023-04-10 20:12:52 -0300

    Criação da classe AgendaController

---
+++ src/main/java/com/example/SCCO_MVC/api/controller/AgendaController.java
@@ -0,0 +1,29 @@
+package com.example.SCCO_MVC.api.controller;
+
+import com.example.SCCO_MVC.api.dto.AgendaDTO;
+import com.example.SCCO_MVC.model.entity.Agenda;
+import com.example.SCCO_MVC.service.AgendaService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+@RestController
+@RequestMapping("/api/v1/agendas")
+@RequiredArgsConstructor
+
+public class AgendaController {
+    private final AgendaService service;
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Agenda> dias = service.getAgendas();
+        return ResponseEntity.ok(dias.stream().map(AgendaDTO::create).collect(Collectors.toList()));
+    }
+}
