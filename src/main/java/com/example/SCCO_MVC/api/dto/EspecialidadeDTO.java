4c62b776e75030a1ce3516049e84727c9354a603
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-10 20:41:04 -0300

    Adição de anotações nas classes DTO

--- src/main/java/com/example/SCCO_MVC/api/dto/EspecialidadeDTO.java
+++ src/main/java/com/example/SCCO_MVC/api/dto/EspecialidadeDTO.java
@@ -1,8 +1,14 @@
 package com.example.SCCO_MVC.api.dto;
 
 import com.example.SCCO_MVC.model.entity.Especialidade;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EspecialidadeDTO {
     private Long id;
     private String nome;
--- src/main/java/com/example/SCCO_MVC/api/dto/SecretariaDTO.java
+++ src/main/java/com/example/SCCO_MVC/api/dto/SecretariaDTO.java
@@ -1,8 +1,14 @@
 package com.example.SCCO_MVC.api.dto;
 
 import com.example.SCCO_MVC.model.entity.Secretaria;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SecretariaDTO {
     private Long id;
     private String nome;
