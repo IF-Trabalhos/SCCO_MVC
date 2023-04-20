84d916e5aa872e2664124befdfe2c9aeaea11aee
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-19 22:59:49 -0300

    Criação do post em Procedimento

--- src/main/java/com/example/SCCO_MVC/api/controller/ProcedimentoController.java
+++ src/main/java/com/example/SCCO_MVC/api/controller/ProcedimentoController.java
@@ -2,15 +2,16 @@ package com.example.SCCO_MVC.api.controller;
 
 
 import com.example.SCCO_MVC.api.dto.ProcedimentoDTO;
-import com.example.SCCO_MVC.model.entity.Procedimento;
+import com.example.SCCO_MVC.exception.RegraNegocioException;
+import com.example.SCCO_MVC.model.entity.*;
+import com.example.SCCO_MVC.service.EspecialidadeService;
 import com.example.SCCO_MVC.service.ProcedimentoService;
+import com.example.SCCO_MVC.service.TratamentoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProcedimentoController {
     private final ProcedimentoService service;
+    private final TratamentoService tratamentoService;
+    private final EspecialidadeService especialidadeService;
 
     @GetMapping
     public ResponseEntity get(){
@@ -34,6 +37,38 @@ public class ProcedimentoController {
             return  new ResponseEntity("Procedimento não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(procedimentos.map(ProcedimentoDTO::create));
+    }
 
+    @PostMapping
+    public ResponseEntity post(@RequestBody ProcedimentoDTO dto){
+        try{
+            Procedimento procedimento = converter(dto);
+            procedimento = service.salvar(procedimento);
+            return new ResponseEntity(procedimento, HttpStatus.CREATED);
+        }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    public Procedimento converter (ProcedimentoDTO dto){
+        ModelMapper modelMapper = new ModelMapper();
+        Procedimento procedimento = modelMapper.map(dto, Procedimento.class);
+        if (dto.getTratamentoId() != null) {
+            Optional<Tratamento> tratamento = tratamentoService.getTratamentoById
+                    (dto.getTratamentoId());
+            if (!tratamento.isPresent()) {
+                procedimento.setTratamento(null);
+            } else {
+                procedimento.setTratamento(tratamento.get());
+            }
+        }
+        if (dto.getEspecialidadeId() != null) {
+            Optional<Especialidade> especialidade = especialidadeService.getEspecialidadeById(dto.getEspecialidadeId());
+            if (!especialidade.isPresent()) {
+                procedimento.setEspecialidade(null);
+            } else {
+                procedimento.setEspecialidade(especialidade.get());
+            }
+        }
+        return procedimento;
     }
 }
