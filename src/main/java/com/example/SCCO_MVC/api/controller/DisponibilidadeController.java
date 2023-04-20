462fa2b07753f0d914aa7c752ee2aa79defb48f3
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-19 22:55:06 -0300

    Criação do post em Disponibilidade

--- src/main/java/com/example/SCCO_MVC/api/controller/DisponibilidadeController.java
+++ src/main/java/com/example/SCCO_MVC/api/controller/DisponibilidadeController.java
@@ -1,16 +1,18 @@
 package com.example.SCCO_MVC.api.controller;
 
+import com.example.SCCO_MVC.api.dto.DentistaDTO;
 import com.example.SCCO_MVC.api.dto.DisponibilidadeDTO;
+import com.example.SCCO_MVC.exception.RegraNegocioException;
+import com.example.SCCO_MVC.model.entity.Dentista;
 import com.example.SCCO_MVC.model.entity.Disponibilidade;
+import com.example.SCCO_MVC.model.entity.Endereco;
 import com.example.SCCO_MVC.service.DisponibilidadeService;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +39,21 @@ public class DisponibilidadeController {
             return  new ResponseEntity("Disponibilidade não encontrada", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(disponibilidade.map(DisponibilidadeDTO::create));
+    }
 
+    @PostMapping
+    public ResponseEntity post(@RequestBody DisponibilidadeDTO dto){
+        try{
+            Disponibilidade disponibilidade = converter(dto);
+            disponibilidade = service.salvar(disponibilidade);
+            return new ResponseEntity(disponibilidade, HttpStatus.CREATED);
+        }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    public Disponibilidade converter(DisponibilidadeDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Disponibilidade disponibilidade = modelMapper.map(dto, Disponibilidade.class);
+        return disponibilidade;
     }
 }
