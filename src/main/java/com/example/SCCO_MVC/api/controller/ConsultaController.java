ddca4f653f1c99fa8c95df4028f981eb9e9027ea
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-19 22:47:45 -0300

    Criação do post em Consulta

--- src/main/java/com/example/SCCO_MVC/api/controller/ConsultaController.java
+++ src/main/java/com/example/SCCO_MVC/api/controller/ConsultaController.java
@@ -1,16 +1,19 @@
 package com.example.SCCO_MVC.api.controller;
 
 
+import com.example.SCCO_MVC.api.dto.AgendaDTO;
 import com.example.SCCO_MVC.api.dto.ConsultaDTO;
-import com.example.SCCO_MVC.model.entity.Consulta;
+import com.example.SCCO_MVC.exception.RegraNegocioException;
+import com.example.SCCO_MVC.model.entity.*;
 import com.example.SCCO_MVC.service.ConsultaService;
+import com.example.SCCO_MVC.service.DentistaService;
+import com.example.SCCO_MVC.service.PacienteService;
+import com.example.SCCO_MVC.service.TratamentoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +24,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ConsultaController {
     private final ConsultaService service;
+    private final TratamentoService tratamentoService;
+    private final DentistaService dentistaService;
+    private final PacienteService pacienteService;
 
     @GetMapping
     public ResponseEntity get(){
@@ -34,6 +40,46 @@ public class ConsultaController {
             return  new ResponseEntity("Consulta não encontrada", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(consultas.map(ConsultaDTO::create));
+    }
 
+    @PostMapping
+    public ResponseEntity post(@RequestBody ConsultaDTO dto){
+        try{
+            Consulta consulta = converter(dto);
+            consulta = service.salvar(consulta);
+            return new ResponseEntity(consulta, HttpStatus.CREATED);
+        }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    public Consulta converter (ConsultaDTO dto){
+        ModelMapper modelMapper = new ModelMapper();
+        Consulta consulta = modelMapper.map(dto, Consulta.class);
+        if (dto.getTratamentoId() != null) {
+            Optional<Tratamento> tratamento = tratamentoService.getTratamentoById
+                    (dto.getTratamentoId());
+            if (!tratamento.isPresent()) {
+                consulta.setTratamento(null);
+            } else {
+                consulta.setTratamento(tratamento.get());
+            }
+        }
+        if (dto.getDentistaId() != null) {
+            Optional<Dentista> dentista = dentistaService.getDentistaById(dto.getDentistaId());
+            if (!dentista.isPresent()) {
+                consulta.setDentista(null);
+            } else {
+                consulta.setDentista(dentista.get());
+            }
+        }
+        if (dto.getPacienteId() != null) {
+            Optional<Paciente> paciente = pacienteService.getPacienteById(dto.getPacienteId());
+            if (!paciente.isPresent()) {
+                consulta.setPaciente(null);
+            } else {
+                consulta.setPaciente(paciente.get());
+            }
+        }
+        return consulta;
     }
 }
