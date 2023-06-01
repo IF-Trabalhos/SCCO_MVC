e8217fcbb574ec3bb85ff01530bd3135d0cec5c2
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-06-01 18:40:41 -0300

    Update ConsultaRepository.java

--- src/main/java/com/example/SCCO_MVC/model/repository/ConsultaRepository.java
+++ src/main/java/com/example/SCCO_MVC/model/repository/ConsultaRepository.java
@@ -2,6 +2,11 @@ package com.example.SCCO_MVC.model.repository;
 
 import com.example.SCCO_MVC.model.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    @Query(value = "SELECT a FROM Consulta a WHERE a.dentista.id = :id")
+    List<Consulta> getConsultaByDentistaId(Long id);
 }
