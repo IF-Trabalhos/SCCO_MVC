3e0ab91d80306e9276bfdd0876a183588326b9a5
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-06-02 19:56:36 -0300

    Busca no banco de dados

--- src/main/java/com/example/SCCO_MVC/model/repository/ConsultaRepository.java
+++ src/main/java/com/example/SCCO_MVC/model/repository/ConsultaRepository.java
@@ -4,9 +4,12 @@ import com.example.SCCO_MVC.model.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     @Query(value = "SELECT a FROM Consulta a WHERE a.dentista.id = :id")
-    List<Consulta> getConsultaByDentistaId(Long id);
+    List<Consulta> getConsultasByDentistaId(Long id);
+    @Query(value = "SELECT ad FROM Consulta ad WHERE ad.data = :data")
+    List<Consulta> getConsultasByData(Date data);
 }
